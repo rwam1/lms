ca56f5db5bf233a65936889de673ce4d3103bdad
parthbhagadhavi <pgadhavi878@gmail.com>
2025-03-28 12:36:18 +0530

    update color

--- src/Button2.jsx
+++ src/Button2.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Button2() {
   return (
     <div>
-        <button style={{color:'blueviolet'}}>Bhavin</button>
+        <button style={{color:'red'}}>Bhavin</button>
     </div>
   )
 }
